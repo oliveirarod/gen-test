f755763a26b2a8178834cbfcca6b5f9225f5decc
oliveirarod <rodrigo.oliveira6446@gmail.com>
2023-04-28 13:42:11 -0300

    Cria estilos globais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from "@angular/core";
 import { MatDrawer } from "@angular/material/sidenav";
 
+interface MatListItemModel {
+  name: string;
+  router: string;
+}
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "gen-test";
+  matListItems: MatListItemModel[] = [
+    { name: "Welcome page", router: "/welcome-page" },
+    { name: "Posts page", router: "/display-data" },
+    { name: "Thanks page", router: "/thanks-page" },
+  ];
 
   // Method to open / close side menu
   sideMenuToggle(menu: MatDrawer) {
